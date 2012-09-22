4a71368d6cea4c7f77ab48365c4230650651f095
Andreas Ericsson <ae@op5.se>
2012-09-22 13:46:07 +0200

    Fix #include paths to Nagios' headers
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- src/nagios.h
+++ src/nagios.h
@@ -28,13 +28,13 @@
 #include "config.h"
 
 #define NSCORE
-#include "nagios/objects.h"
-#include "nagios/nagios.h"
-#include "nagios/nebstructs.h"
-#include "nagios/neberrors.h"
-#include "nagios/broker.h"
-#include "nagios/nebmodules.h"
-#include "nagios/nebcallbacks.h"
+#include <nagios/objects.h>
+#include <nagios/nagios.h>
+#include <nagios/nebstructs.h>
+#include <nagios/neberrors.h>
+#include <nagios/broker.h>
+#include <nagios/nebmodules.h>
+#include <nagios/nebcallbacks.h>
 
 #endif // nagios_h
 
