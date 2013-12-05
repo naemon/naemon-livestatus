dcddf38811be3ac1b35137f431ec8d55dc821827
Sven Nierlein <Sven.Nierlein@consol.de>
2013-12-05 23:20:57 +0100

    removed api and build files
    Signed-off-by: Sven Nierlein <sven@consol.de>

--- api/python/__init__.py
+++
@@ -1,9 +0,0 @@
-from livestatus import SingleSiteConnection, MultiSiteConnection, MKLivestatusException, MKLivestatusSocketError, MKLivestatusSocketClosed, MKLivestatusQueryError, MKLivestatusNotFoundError
-__all__ = [
-    'SingleSiteConnection',
-    'MultiSiteConnection',
-    'MKLivestatusException',
-    'MKLivestatusSocketError',
-    'MKLivestatusSocketClosed',
-    'MKLivestatusQueryError',
-    'MKLivestatusNotFoundError']
