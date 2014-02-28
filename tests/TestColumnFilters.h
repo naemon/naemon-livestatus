10a773ccb2d9667010d737cce59bc503b2cd33d1
Anton Lofgren <alofgren@op5.com>
2014-02-28 12:39:29 +0100

    build: Add plumbing for running cppunit tests
    This patch adds support for running CPPUnit test cases on `make check'.
    
    It also adds some (basic) test cases for all supported string column
    filter operations.
    
    Change-Id: I6f2fe198343687d95de4c72d5d34f42a8bbaff66
    Signed-off-by: Anton Lofgren <alofgren@op5.com>

---
+++ tests/TestColumnFilters.h
@@ -0,0 +1,23 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+class StringColumnFilterTest : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE( StringColumnFilterTest );
+	CPPUNIT_TEST( testEqualOp );
+	CPPUNIT_TEST( testRegexOp );
+	CPPUNIT_TEST( testEqualIcaseOp );
+	CPPUNIT_TEST( testRegexIcaseOp );
+	CPPUNIT_TEST( testGreaterOp );
+	CPPUNIT_TEST( testLessOp );
+	CPPUNIT_TEST_SUITE_END();
+
+	public:
+	void setUp();
+	void tearDown();
+	void testEqualOp();
+	void testRegexOp();
+	void testEqualIcaseOp();
+	void testRegexIcaseOp();
+	void testGreaterOp();
+	void testLessOp();
+};
