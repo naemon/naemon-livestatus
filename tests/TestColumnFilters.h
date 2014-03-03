98d896090c70cebdd325db9da9482b54299280e9
Anton Lofgren <alofgren@op5.com>
2014-03-03 13:32:47 +0100

    tests: Test regex filters with unicode characters
    This verifies #8203.
    
    Change-Id: I5ef4ead5deb9853a7bb824596f04698cf23f1b6f
    Signed-off-by: Anton Lofgren <alofgren@op5.com>

--- tests/TestColumnFilters.h
+++ tests/TestColumnFilters.h
@@ -5,8 +5,10 @@ class StringColumnFilterTest : public CppUnit::TestFixture
 	CPPUNIT_TEST_SUITE( StringColumnFilterTest );
 	CPPUNIT_TEST( testEqualOp );
 	CPPUNIT_TEST( testRegexOp );
+	CPPUNIT_TEST( testRegexNonAscii );
 	CPPUNIT_TEST( testEqualIcaseOp );
 	CPPUNIT_TEST( testRegexIcaseOp );
+	CPPUNIT_TEST( testRegexIcaseNonAscii );
 	CPPUNIT_TEST( testGreaterOp );
 	CPPUNIT_TEST( testLessOp );
 	CPPUNIT_TEST_SUITE_END();
@@ -16,8 +18,10 @@ class StringColumnFilterTest : public CppUnit::TestFixture
 	void tearDown();
 	void testEqualOp();
 	void testRegexOp();
+	void testRegexNonAscii();
 	void testEqualIcaseOp();
 	void testRegexIcaseOp();
+	void testRegexIcaseNonAscii();
 	void testGreaterOp();
 	void testLessOp();
 };
