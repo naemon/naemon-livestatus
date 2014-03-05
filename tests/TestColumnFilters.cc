ea3630459f02274a425fcaa9f8faf23274bd32ca
Anton Lofgren <alofgren@op5.com>
2014-03-05 10:37:23 +0100

    tests: Free memory in tests
    This way, we can run valgrind and verify that the columnfilters don't
    leak without a lot of noise.
    
    Change-Id: I6298fb5ac081dbe4bf192322ec713ce827451c07
    Signed-off-by: Anton Lofgren <alofgren@op5.com>

--- tests/TestColumnFilters.cc
+++ tests/TestColumnFilters.cc
@@ -3,6 +3,7 @@
 #include <cppunit/ui/text/TextTestRunner.h>
 #include <string.h>
 #include <locale.h>
+#include <stdlib.h>
 #include <vector>
 #include "TestColumnFilters.h"
 #include "../src/OffsetStringColumn.h"
@@ -21,7 +22,6 @@
  */
 
 CPPUNIT_TEST_SUITE_REGISTRATION( StringColumnFilterTest );
-#define DUMMY_COLUMN new OffsetStringColumn("some-column", "a description for the column", 0)
 void
 StringColumnFilterTest::setUp()
 {
@@ -36,58 +36,78 @@ void
 StringColumnFilterTest::testEqualOp()
 {
 	char * refstring = strdup("Tinky-Winky");
-	string * value_eq = new string("Tinky-Winky");
-	string * value_neq = new string("Po");
-	StringColumnFilter *scf = new StringColumnFilter(DUMMY_COLUMN, OP_EQUAL, refstring);
-	CPPUNIT_ASSERT(scf->accepts(value_eq));
-	CPPUNIT_ASSERT(!scf->accepts(value_neq));
+	string value_eq("Tinky-Winky");
+	string value_neq("Po");
+	OffsetStringColumn * column = new OffsetStringColumn("some-column", "a description for the column", 0);
+	StringColumnFilter * scf = new StringColumnFilter(column, OP_EQUAL, refstring);
+	CPPUNIT_ASSERT(scf->accepts(&value_eq));
+	CPPUNIT_ASSERT(!scf->accepts(&value_neq));
+	delete scf;
+	delete column;
+	free(refstring);
 }
 
 void
 StringColumnFilterTest::testRegexOp()
 {
 	char * refstring = strdup("Dell open");
-	const char *values[] = {"my Dell open manage service", "iDell open manage", "Dell open manage global", "Dell open manage storage"};
+	const char *values[] = {"my Dell open manage service", "i Dell open manage", "Dell open manage global", "Dell open manage storage"};
 	vector<string> value_match_v (values, values+(sizeof(values)/sizeof(values[0])));
-	string * value_nomatch = new string("Dipsy");
-	StringColumnFilter *scf = new StringColumnFilter(DUMMY_COLUMN, OP_REGEX, refstring);
+	string value_nomatch("Dipsy");
+	OffsetStringColumn * column = new OffsetStringColumn("some-column", "a description for the column", 0);
+	StringColumnFilter *scf = new StringColumnFilter(column, OP_REGEX, refstring);
 	for (vector<string>::iterator it = value_match_v.begin(); it != value_match_v.end(); ++it) {
 		CPPUNIT_ASSERT(scf->accepts(&*it));
 	}
-	CPPUNIT_ASSERT(!scf->accepts(value_nomatch));
+	CPPUNIT_ASSERT(!scf->accepts(&value_nomatch));
+	delete scf;
+	delete column;
+	free(refstring);
 }
 
 void
 StringColumnFilterTest::testRegexNonAscii()
 {
 	char * refstring = strdup("^.*Յ$");
-	string * value_match = new string("Capital yi: Յ");
-	string * value_nomatch = new string("Small yi: յ");
-	StringColumnFilter *scf = new StringColumnFilter(DUMMY_COLUMN, OP_REGEX, refstring);
-	CPPUNIT_ASSERT(scf->accepts(value_match));
-	CPPUNIT_ASSERT(!scf->accepts(value_nomatch));
+	string value_match("Capital yi: Յ");
+	string value_nomatch("Small yi: յ");
+	OffsetStringColumn * column = new OffsetStringColumn("some-column", "a description for the column", 0);
+	StringColumnFilter *scf = new StringColumnFilter(column, OP_REGEX, refstring);
+	CPPUNIT_ASSERT(scf->accepts(&value_match));
+	CPPUNIT_ASSERT(!scf->accepts(&value_nomatch));
+	delete scf;
+	delete column;
+	free(refstring);
 }
 
 void
 StringColumnFilterTest::testEqualIcaseOp()
 {
 	char * refstring = strdup("Tinky-Winky");
-	string * value_match = new string("tInkY-WinKy");
-	string * value_nomatch = new string("DIPsY");
-	StringColumnFilter *scf = new StringColumnFilter(DUMMY_COLUMN, OP_EQUAL_ICASE, refstring);
-	CPPUNIT_ASSERT(scf->accepts(value_match));
-	CPPUNIT_ASSERT(!scf->accepts(value_nomatch));
+	string value_match("tInkY-WinKy");
+	string value_nomatch("DIPsY");
+	OffsetStringColumn * column = new OffsetStringColumn("some-column", "a description for the column", 0);
+	StringColumnFilter *scf = new StringColumnFilter(column, OP_EQUAL_ICASE, refstring);
+	CPPUNIT_ASSERT(scf->accepts(&value_match));
+	CPPUNIT_ASSERT(!scf->accepts(&value_nomatch));
+	delete scf;
+	delete column;
+	free(refstring);
 }
 
 void
 StringColumnFilterTest::testRegexIcaseOp()
 {
 	char * refstring = strdup("^Ti[a-z]ky-Winky$");
-	string * value_match = new string("tiskY-wiNkY");
-	string * value_nomatch = new string("JUGGERNAUT");
-	StringColumnFilter *scf = new StringColumnFilter(DUMMY_COLUMN, OP_REGEX_ICASE, refstring);
-	CPPUNIT_ASSERT(scf->accepts(value_match));
-	CPPUNIT_ASSERT(!scf->accepts(value_nomatch));
+	string value_match("tiskY-wiNkY");
+	string value_nomatch("JUGGERNAUT");
+	OffsetStringColumn * column = new OffsetStringColumn("some-column", "a description for the column", 0);
+	StringColumnFilter *scf = new StringColumnFilter(column, OP_REGEX_ICASE, refstring);
+	CPPUNIT_ASSERT(scf->accepts(&value_match));
+	CPPUNIT_ASSERT(!scf->accepts(&value_nomatch));
+	delete scf;
+	delete column;
+	free(refstring);
 }
 
 /**
@@ -97,33 +117,45 @@ void
 StringColumnFilterTest::testRegexIcaseNonAscii()
 {
 	char * refstring = strdup("te tse incoming: Ҵ");
-	string * value_match = new string("Capital te tse incoming: Ҵ");
-	string * value_match2 = new string("Small te tse incoming: ҵ");
-	StringColumnFilter *scf = new StringColumnFilter(DUMMY_COLUMN, OP_REGEX_ICASE, refstring);
-	CPPUNIT_ASSERT(scf->accepts(value_match));
-	CPPUNIT_ASSERT(scf->accepts(value_match2));
+	string value_match("Capital te tse incoming: Ҵ");
+	string value_match2("Small te tse incoming: ҵ");
+	OffsetStringColumn * column = new OffsetStringColumn("some-column", "a description for the column", 0);
+	StringColumnFilter *scf = new StringColumnFilter(column, OP_REGEX_ICASE, refstring);
+	CPPUNIT_ASSERT(scf->accepts(&value_match));
+	CPPUNIT_ASSERT(scf->accepts(&value_match2));
+	delete scf;
+	delete column;
+	free(refstring);
 }
 
 void
 StringColumnFilterTest::testGreaterOp()
 {
 	char * refstring = strdup("a relatively low value string");
-	string * value_less = new string("littlestringer");
-	string * value_greater = new string("AN EXTREEEEEMELY VALUABLE STRING WITH LOTS OF SHOUTING");
-	StringColumnFilter *scf = new StringColumnFilter(DUMMY_COLUMN, OP_GREATER, refstring);
-	CPPUNIT_ASSERT(scf->accepts(value_less));
-	CPPUNIT_ASSERT(!scf->accepts(value_greater));
+	string value_less("littlestringer");
+	string value_greater("AN EXTREEEEEMELY VALUABLE STRING WITH LOTS OF SHOUTING");
+	OffsetStringColumn * column = new OffsetStringColumn("some-column", "a description for the column", 0);
+	StringColumnFilter *scf = new StringColumnFilter(column, OP_GREATER, refstring);
+	CPPUNIT_ASSERT(scf->accepts(&value_less));
+	CPPUNIT_ASSERT(!scf->accepts(&value_greater));
+	delete scf;
+	delete column;
+	free(refstring);
 }
 
 void
 StringColumnFilterTest::testLessOp()
 {
 	char * refstring = strdup("a relatively low value string");
-	string * value_less = new string("littlestringer");
-	string * value_greater = new string("AN EXTREEEEEMELY VALUABLE STRING WITH LOTS OF SHOUTING");
-	StringColumnFilter *scf = new StringColumnFilter(DUMMY_COLUMN, OP_LESS, refstring);
-	CPPUNIT_ASSERT(!scf->accepts(value_less));
-	CPPUNIT_ASSERT(scf->accepts(value_greater));
+	string value_less("littlestringer");
+	string value_greater("AN EXTREEEEEMELY VALUABLE STRING WITH LOTS OF SHOUTING");
+	OffsetStringColumn * column = new OffsetStringColumn("some-column", "a description for the column", 0);
+	StringColumnFilter *scf = new StringColumnFilter(column, OP_LESS, refstring);
+	CPPUNIT_ASSERT(!scf->accepts(&value_less));
+	CPPUNIT_ASSERT(scf->accepts(&value_greater));
+	delete scf;
+	delete column;
+	free(refstring);
 }
 
 int main(int argc, char * argv[])
