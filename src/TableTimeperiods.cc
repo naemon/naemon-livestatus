f6af24fff41d9d4ac45f077f9db62211c7dffa93
Max Sikstrom <max.sikstrom@op5.com>
2015-09-07 11:20:14 +0200

    Expose timeperiod information
    Timeperiods only contained name and alias, and also if it was currently
    active. But no information about the schedule, to analyze and present
    over time.
    
    Expose that information, so we can use that as base for reports.
    
    Resolves livestatus part of MON-8443
    
    Change-Id: I7c0bc21334210b650185b3ca476c296e52e60092
    Signed-off-by: Max Sikstrom <max.sikstrom@op5.com>

--- src/TableTimeperiods.cc
+++ src/TableTimeperiods.cc
@@ -27,35 +27,71 @@
 #include "OffsetStringColumn.h"
 #include "OffsetIntColumn.h"
 #include "OffsetTimeperiodColumn.h"
+#include "TimeperiodDaysColumn.h"
+#include "TimeperiodExclusionColumn.h"
+#include "TimeperiodExceptionsColumn.h"
 #include "TableTimeperiods.h"
 
-TableTimeperiods::TableTimeperiods()
-{
+TableTimeperiods::TableTimeperiods() {
     addColumns(this, "", -1);
 }
 
-
-void TableTimeperiods::addColumns(Table *table, string prefix, int indirect_offset)
-{
+void TableTimeperiods::addColumns(Table *table, string prefix,
+        int indirect_offset) {
     timeperiod tp;
-    char *ref = (char *)&tp;
-    table->addColumn(new OffsetIntColumn(prefix + "id",
-                "Timeperiod id", (char *)(&tp.id) - ref, indirect_offset));
-    table->addColumn(new OffsetStringColumn(prefix + "name",
-                "The name of the timeperiod", (char *)(&tp.name) - ref, indirect_offset));
-    table->addColumn(new OffsetStringColumn(prefix + "alias",
-                "The alias of the timeperiod", (char *)(&tp.alias) - ref, indirect_offset));
-    table->addColumn(new OffsetTimeperiodColumn(prefix + "in",
-                "Wether we are currently in this period (0/1)", -1, indirect_offset));
-    // TODO: add days and exceptions
+    char *ref = (char *) &tp;
+    table->addColumn(
+            new OffsetIntColumn(prefix + "id", "Timeperiod id",
+                    (char *) (&tp.id) - ref, indirect_offset));
+    table->addColumn(
+            new OffsetStringColumn(prefix + "name",
+                    "The name of the timeperiod", (char *) (&tp.name) - ref,
+                    indirect_offset));
+    table->addColumn(
+            new OffsetStringColumn(prefix + "alias",
+                    "The alias of the timeperiod", (char *) (&tp.alias) - ref,
+                    indirect_offset));
+    table->addColumn(
+            new TimeperiodDaysColumn(prefix + "days",
+                    "Time per days, as 7 lists (sun-sat) of a list of even number of elements, representing start,stop,start,stop... in seconds since midnight",
+                    indirect_offset));
+
+    table->addColumn(
+            new TimeperiodExceptionsColumn(prefix + "exceptions_calendar_dates",
+                    "List of objects representing exceptions for DATERANGE_CALENDAR_DATE. For time ranges, see \"days\" column.",
+                    indirect_offset, DATERANGE_CALENDAR_DATE));
+    table->addColumn(
+            new TimeperiodExceptionsColumn(prefix + "exceptions_month_date",
+                    "List of objects representing exceptions for DATERANGE_MONTH_DATE. For time ranges, see \"days\" column.",
+                    indirect_offset, DATERANGE_MONTH_DATE));
+    table->addColumn(
+            new TimeperiodExceptionsColumn(prefix + "exceptions_month_day",
+                    "List of objects representing exceptions for DATERANGE_MONTH_DAY. For time ranges, see \"days\" column.",
+                    indirect_offset, DATERANGE_MONTH_DAY));
+    table->addColumn(
+            new TimeperiodExceptionsColumn(prefix + "exceptions_month_week_day",
+                    "List of objects representing exceptions for DATERANGE_MONTH_WEEK_DAY. For time ranges, see \"days\" column.",
+                    indirect_offset, DATERANGE_MONTH_WEEK_DAY));
+    table->addColumn(
+            new TimeperiodExceptionsColumn(prefix + "exceptions_week_day",
+                    "List of objects representing exceptions for DATERANGE_WEEK_DAY. For time ranges, see \"days\" column.",
+                    indirect_offset, DATERANGE_WEEK_DAY));
+
+    table->addColumn(
+            new TimeperiodExclusionColumn(prefix + "exclusions",
+                    "Exclusions for this timeperiod",
+                    (char *) (&tp.exclusions) - ref, indirect_offset));
+    table->addColumn(
+            new OffsetTimeperiodColumn(prefix + "in",
+                    "Whether we are currently in this period (0/1)", -1,
+                    indirect_offset));
 }
 
-
-void TableTimeperiods::answerQuery(Query *query)
-{
+void TableTimeperiods::answerQuery(Query *query) {
     timeperiod *tp = timeperiod_list;
     while (tp) {
-        if (!query->processDataset(tp)) break;
+        if (!query->processDataset(tp))
+            break;
         tp = tp->next;
     }
 }
---
+++ src/TimeperiodExceptionsColumn.cc
@@ -0,0 +1,124 @@
+// +------------------------------------------------------------------+
+// |             ____ _               _        __  __ _  __           |
+// |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+// |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+// |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+// |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+// |                                                                  |
+// | Copyright Mathias Kettner 2012             mk@mathias-kettner.de |
+// +------------------------------------------------------------------+
+//
+// This file is part of Check_MK.
+// The official homepage is at http://mathias-kettner.de/check_mk.
+//
+// check_mk is free software;  you can redistribute it and/or modify it
+// under the  terms of the  GNU General Public License  as published by
+// the Free Software Foundation in version 2.  check_mk is  distributed
+// in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+// out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+// PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+// ails.  You should have  received  a copy of the  GNU  General Public
+// License along with GNU Make; see the file  COPYING.  If  not,  write
+// to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+// Boston, MA 02110-1301 USA.
+
+#include "TimeperiodExceptionsColumn.h"
+#include "OringFilter.h"
+#include "nagios.h"
+#include "Query.h"
+
+#define OUTPUT_DICT_ENTRY(_Q, _O, _F) do { \
+        (_Q)->outputString(#_F); \
+        (_Q)->outputDictValueSeparator(); \
+        (_Q)->outputInteger((_O)->_F); \
+        (_Q)->outputDictSeparator(); \
+    } while(0)
+
+void TimeperiodExceptionsColumn::output(void *data, Query *query) {
+    timeperiod *tp;
+
+    daterange *dr;
+    bool first_dr;
+
+    timerange *tr;
+    bool first_tr;
+
+    data = shiftPointer(data);
+    if (!data)
+        return;
+    tp = (timeperiod*) data;
+
+    query->outputBeginList();
+    first_dr = true;
+    for (dr = tp->exceptions[_daterange]; dr; dr = dr->next) {
+        if (!first_dr)
+            query->outputListSeparator();
+        first_dr = false;
+
+        query->outputBeginDict();
+
+        /* Output of variables */
+        switch(_daterange) {
+        case DATERANGE_CALENDAR_DATE:
+            OUTPUT_DICT_ENTRY(query, dr, syear);
+            OUTPUT_DICT_ENTRY(query, dr, smon);
+            OUTPUT_DICT_ENTRY(query, dr, smday);
+            OUTPUT_DICT_ENTRY(query, dr, eyear);
+            OUTPUT_DICT_ENTRY(query, dr, emon);
+            OUTPUT_DICT_ENTRY(query, dr, emday);
+            break;
+        case DATERANGE_MONTH_DATE:
+            OUTPUT_DICT_ENTRY(query, dr, smon);
+            OUTPUT_DICT_ENTRY(query, dr, smday);
+            OUTPUT_DICT_ENTRY(query, dr, emon);
+            OUTPUT_DICT_ENTRY(query, dr, emday);
+            break;
+        case DATERANGE_MONTH_DAY:
+            OUTPUT_DICT_ENTRY(query, dr, smday);
+            OUTPUT_DICT_ENTRY(query, dr, emday);
+            break;
+        case DATERANGE_MONTH_WEEK_DAY:
+            OUTPUT_DICT_ENTRY(query, dr, smon);
+            OUTPUT_DICT_ENTRY(query, dr, swday);
+            OUTPUT_DICT_ENTRY(query, dr, swday_offset);
+            OUTPUT_DICT_ENTRY(query, dr, emon);
+            OUTPUT_DICT_ENTRY(query, dr, ewday);
+            OUTPUT_DICT_ENTRY(query, dr, ewday_offset);
+            break;
+        case DATERANGE_WEEK_DAY:
+            OUTPUT_DICT_ENTRY(query, dr, swday);
+            OUTPUT_DICT_ENTRY(query, dr, swday_offset);
+            OUTPUT_DICT_ENTRY(query, dr, ewday);
+            OUTPUT_DICT_ENTRY(query, dr, ewday_offset);
+            break;
+        }
+        OUTPUT_DICT_ENTRY(query, dr, skip_interval);
+
+        /* Output time ranges */
+        query->outputString("times");
+        query->outputDictValueSeparator();
+        first_tr = true;
+        query->outputBeginSublist();
+        for (tr = dr->times; tr; tr = tr->next) {
+            if (!first_tr)
+                query->outputSublistSeparator();
+            first_tr = false;
+
+            query->outputInteger(tr->range_start);
+            query->outputSublistSeparator();
+            query->outputInteger(tr->range_end);
+        }
+        query->outputEndSublist();
+
+        query->outputEndDict();
+    }
+    query->outputEndList();
+}
+
+Filter *TimeperiodExceptionsColumn::createFilter(int opid, char *value) {
+    // Nothing matches (except if negated)
+    if (opid < 0)
+        return new AndingFilter();
+    return new OringFilter();
+}
+
