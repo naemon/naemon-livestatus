65dda13fc8f820d8275f850bd7290a93525ca5fd
Sven Nierlein <Sven.Nierlein@consol.de>
2026-09-08 16:57:53 +0200

    tests: drop RSpec dependency from cucumber steps
    Cucumber step definitions used RSpec matchers (.should, expect().to match),
    which the newer cucumber package on ubuntu-26.04 no longer loads, causing
    'undefined method should for an instance of String'. Replace them with plain
    Ruby assertions, and require json + use JSON.parse for the clobber step.

--- tests/features/step_definitions/livestatus.rb
+++ tests/features/step_definitions/livestatus.rb
@@ -9,14 +9,15 @@ end
 Then /^I should see the following raw livestatus response$/ do |table|
   response = @naemon.brokers[:livestatus].last_response()
   table.raw.each_with_index do |line, idx|
-    response[idx].should == "\"#{line[0]}\"".undump
+    expected = "\"#{line[0]}\"".undump
+    raise "row #{idx}: expected #{expected.inspect}, got #{response[idx].inspect}" unless response[idx] == expected
   end
 end
 
 Then /^I should see the following livestatus response$/ do |table|
   response = @naemon.brokers[:livestatus].last_response()
   table.raw.each_with_index do |line, idx|
-    response[idx].should == line[0]
+    raise "row #{idx}: expected #{line[0].inspect}, got #{response[idx].inspect}" unless response[idx] == line[0]
   end
 end
 
@@ -24,13 +25,13 @@ Then /^I should see the following livestatus response, ignoring whitespace$/ do 
   response = @naemon.brokers[:livestatus].last_response()
   processed_response = response.join("").gsub(/[ \t\n\r]/,"")
   processed_output = output.gsub(/[ \t\n\r]/,"")
-  processed_response.should == processed_output
+  raise "expected #{processed_output.inspect}, got #{processed_response.inspect}" unless processed_response == processed_output
 end
 
 Then /^I should see the following livestatus response, using regular expression$/ do |output|
   response = @naemon.brokers[:livestatus].last_response()
   processed_response = response.join("\n")
-  expect(processed_response).to match(output)
+  raise "expected response to match /#{output}/, got #{processed_response.inspect}" unless processed_response.match?(Regexp.new(output))
 end
 
 Given(/^I wait for (\d+) seconds$/) do |secs|
@@ -40,7 +41,7 @@ end
 Then /^I should see at least (\d+) distinct rows in the livestatus response$/ do |min_rows|
   response = @naemon.brokers[:livestatus].last_response()
   distinct = response.uniq
-  expect(distinct.length).to be >= min_rows.to_i
+  raise "expected at least #{min_rows} distinct rows, got #{distinct.length}" unless distinct.length >= min_rows.to_i
 end
 
 Given /^I clobber livestatus with (\d+) queries with (\d+) seconds idle time$/ do |nqueries, idle_time|
@@ -55,9 +56,10 @@ Then /^the slowest query should be no more than (\d+) times slower than the fast
   clobber_data = @naemon.brokers[:livestatus].clobber_data()
   fastest = clobber_data['min_time']
   slowest = clobber_data['max_time']
-  slowest.should < (fastest * factor.to_i)
+  raise "slowest #{slowest} exceeds #{factor}x fastest #{fastest}" unless slowest < (fastest * factor.to_i)
 end
 
 Then /^the average query response time should be no more than (\d+.\d+) seconds$/ do |threshold|
-  @naemon.brokers[:livestatus].clobber_data()['avg_time'].should < threshold.to_f
+  avg = @naemon.brokers[:livestatus].clobber_data()['avg_time']
+  raise "average #{avg} exceeds #{threshold} seconds" unless avg < threshold.to_f
 end
